7ea27c13dc75b301543c57ac9c962ab0c173b741
Divya Maheswaran <divyamaheswaran93@gmail.com>
2023-11-07 20:48:21 -0800

    create part_of_speech_tagging.py
    POS tagging: For a given token it is looking where that token is used within the context of a sentence.
    
    Bonus code:
    1) To count each tag present in the sentence.
    2) Plot frequency distribution of words using MatlabPlot

---
+++ part_of_speech_tagging.py
@@ -0,0 +1,21 @@
+import nltk
+from nltk.tokenize import word_tokenize
+from collections import Counter
+
+sample_text = "This this is a a a a sample text text text text text to to explain the part of speech tagging!"
+
+chunks = word_tokenize(sample_text)
+
+#Let's grammatically classify the sample text using pos_tag
+tagged = nltk.pos_tag(chunks)
+print("Words with tags attached:")
+print(tagged)
+
+#Let's count the each tag present in the sample text using dictionary counter where the words are keys and tags are values.
+print("Tag Counts:")
+counter = Counter(t for word, t in tagged)
+print(counter)
+
+#Let's plot the frequency distribution of words in the sample text
+fd = nltk.FreqDist(chunks)
+fd.plot()
